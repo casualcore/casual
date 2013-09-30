a9e8c651f0a560152873acefbc1cf6db720525b2
Hans Bergkvist <hans.bergkvist@gmail.com>
2013-09-30 22:30:12 +0200

    Some changes to plugin

--- plugin/ngx_http_casual_module.c
+++ plugin/ngx_http_casual_module.c
@@ -20,6 +20,8 @@ static void* ngx_http_casual_create_loc_conf(ngx_conf_t *cf);
 static char* ngx_http_casual_merge_loc_conf(ngx_conf_t *cf, void *parent,
       void *child);
 
+static void ngx_http_casual_input_post_read(ngx_http_request_t *r);
+
 char* handleError(ngx_http_request_t *r);
 
 typedef struct
@@ -69,49 +71,113 @@ ngx_module_t ngx_http_casual_module = { NGX_MODULE_V1,
       NULL, /* exit master */
       NGX_MODULE_V1_PADDING };
 
+static ngx_int_t extractArgument( ngx_http_request_t *r, const char* const argument, u_char* value, const char* const defaultValue)
+{
+   ngx_str_t s;
+   u_char* p;
+   ngx_log_debug(NGX_LOG_DEBUG_ALL, r->connection->log, 0, argument);
+   if (ngx_http_arg(r, (u_char*) argument, strlen(argument), &s) == NGX_OK)
+   {
+      p = ngx_copy(value, s.data, s.len );
+      *p = '\0';
+      ngx_log_debug(NGX_LOG_DEBUG_ALL, r->connection->log, 0, "Using selected value");
+   }
+   else
+   {
+      //
+      // Has defaultvalue
+      //
+      if (defaultValue)
+      {
+         //
+         // Default
+         //
+         ngx_memcpy(value, (u_char*) defaultValue, sizeof(defaultValue) - 1);
+         value[sizeof(defaultValue) - 1] = '\0';
+         ngx_log_debug(NGX_LOG_DEBUG_ALL, r->connection->log, 0, "Using default value");
+      }
+   }
+
+   return NGX_OK;
+}
+
 static ngx_int_t ngx_http_casual_handler(ngx_http_request_t *r)
 {
    ngx_log_debug(NGX_LOG_DEBUG_ALL, r->connection->log, 0,
          "ngx_http_casual_handler.\n");
 
    ngx_int_t rc;
-   ngx_str_t s;
    ngx_chain_t out;
-   u_char* service = 0;
-   u_char* p;
    ngx_buf_t *b;
+   u_char *body = 0;
 
+   //
+   // Only POST supported
+   //
+   if (!(r->method & (NGX_HTTP_POST)))
+   {
+      return NGX_HTTP_NOT_ALLOWED;
+   }
+
+   //
+   // Read configuration
+   //
    ngx_http_casual_loc_conf_t *cglcf;
    cglcf = ngx_http_get_module_loc_conf(r, ngx_http_casual_module);
 
    r->headers_out.status = NGX_HTTP_OK;
 
-   if (ngx_http_arg(r, (u_char*) "service", sizeof("service") - 1, &s) == NGX_OK)
-   {
-      service = ngx_pnalloc(r->pool, s.len + 1);
-      if (service == NULL )
-      {
-         return NGX_ERROR;
-      }
-      p = ngx_copy(service, s.data, s.len );
-      *p++ = '\0';
-      ngx_log_debug(NGX_LOG_DEBUG_ALL, r->connection->log, 0, (char*) service);
-   }
-   else
+   //
+   // Extract parameters from URL
+   //
+   u_char service[100];
+   if ( extractArgument( r, "service", service, 0) != NGX_OK)
    {
       return NGX_ERROR;
    }
 
-   if (!(r->method & (NGX_HTTP_GET | NGX_HTTP_HEAD)))
+   u_char protocol[10];
+   extractArgument( r, "protocol", protocol, "JSON");
+
+   ngx_log_debug(NGX_LOG_DEBUG_ALL, r->connection->log, 0, (char*) service);
+   ngx_log_debug(NGX_LOG_DEBUG_ALL, r->connection->log, 0, (char*) protocol);
+
+   //
+   // Extract body
+   //
+   rc = ngx_http_read_client_request_body(r, ngx_http_casual_input_post_read);
+
+   //
+   // we read data from r->request_body->bufs
+   //
+   if (r->request_body == NULL || r->request_body->bufs == NULL)
    {
-      return NGX_HTTP_NOT_ALLOWED;
+      ngx_log_debug(NGX_LOG_DEBUG_ALL, r->connection->log, 0, (char*) "No data in body.");
+   }
+   else
+   {
+      ngx_log_debug(NGX_LOG_DEBUG_ALL, r->connection->log, 0, (char*) "Data in body.");
    }
 
-   rc = ngx_http_discard_request_body(r);
-
-   if (rc != NGX_OK && rc != NGX_AGAIN)
+   long content_length = r->headers_in.content_length_n;
+   //
+   // Any data from caller?
+   //
+   if (content_length > 0)
    {
-      return rc;
+      ngx_log_debug(NGX_LOG_DEBUG_ALL, r->connection->log, 0, (char*) "Copying body.");
+
+      body = ngx_palloc(r->pool, content_length);
+      ngx_memcpy(body, r->request_body->bufs->buf->start, content_length);
+      ngx_log_debug(NGX_LOG_DEBUG_ALL, r->connection->log, 0, (char*) body);
+   }
+   else
+   {
+      content_length = 0;
+      ngx_log_debug(NGX_LOG_DEBUG_ALL, r->connection->log, 0, (char*) "Empty body.");
+
+      body = ngx_palloc(r->pool, content_length);
+      ngx_log_debug(NGX_LOG_DEBUG_ALL, r->connection->log, 0, (char*) body);
    }
 
    if (r->headers_in.if_modified_since)
@@ -119,16 +185,25 @@ static ngx_int_t ngx_http_casual_handler(ngx_http_request_t *r)
       return NGX_HTTP_NOT_MODIFIED;
    }
 
-   r->headers_out.content_type.len = sizeof("text/plain") - 1;
-   r->headers_out.content_type.data = (u_char *) "text/plain";
+   //
+   // Start preparing the answer
+   //
+   r->headers_out.content_type.len = sizeof("application/json") - 1;
+   r->headers_out.content_type.data = (u_char *) "application/json";
 
    //
    // Call Casual
    //
+   // TODO: Create own method
+   //
    long buffersize = 0;
    char* error = NULL;
-   //char* buffer = tpalloc( "STRING", "", 1024);
-   char* buffer = tpalloc("X_OCTET", "JSON", 1024);
+
+   char* buffer = tpalloc(X_OCTET, (char*)protocol, 1024);
+
+   strncpy(buffer, (const char*)body, content_length + 1);
+   buffer[content_length] = '\0';
+
    if (!buffer)
    {
       error = handleError(r);
@@ -138,13 +213,19 @@ static ngx_int_t ngx_http_casual_handler(ngx_http_request_t *r)
    {
       int cd1 = 0;
 
-      if ((cd1 = tpacall((char*) service, buffer, 0, 0)) == -1)
+      //
+      // Do the call
+      //
+      if ((cd1 = tpacall((char*) service, buffer, content_length, 0)) == -1)
       {
          error = handleError(r);
          buffersize = strlen(error);
       }
       else
       {
+         //
+         // Handle the reply
+         //
          if (tpgetrply(&cd1, &buffer, &buffersize, 0) == -1)
          {
             error = handleError(r);
@@ -153,18 +234,10 @@ static ngx_int_t ngx_http_casual_handler(ngx_http_request_t *r)
       }
    }
 
+   //
+   // Finish packaging the result
+   //
    r->headers_out.content_length_n = buffersize;
-
-   if (r->method == NGX_HTTP_HEAD)
-   {
-      rc = ngx_http_send_header(r);
-
-      if (rc == NGX_ERROR || rc > NGX_OK || r->header_only)
-      {
-         tpfree(buffer);
-         return rc;
-      }
-   }
 
    b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
    if (b == NULL )
@@ -197,6 +270,8 @@ static ngx_int_t ngx_http_casual_handler(ngx_http_request_t *r)
       ngx_memcpy(text, buffer, r->headers_out.content_length_n);
    }
 
+   ngx_log_debug(NGX_LOG_DEBUG_ALL, r->connection->log, 0, (char*) text);
+
    b->pos = text;
    b->last = text + r->headers_out.content_length_n;
 
@@ -211,17 +286,8 @@ static ngx_int_t ngx_http_casual_handler(ngx_http_request_t *r)
    {
       return rc;
    }
-   return ngx_http_output_filter(r, &out);
-}
 
-char* handleError(ngx_http_request_t *r)
-{
-   long size = strlen(tperrnostring(tperrno));
-   char* error = ngx_pcalloc(r->pool, size);
-   strncpy(error, tperrnostring(tperrno), size);
-   ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, error);
-   r->headers_out.status = NGX_HTTP_BAD_REQUEST;
-   return error;
+   return ngx_http_output_filter(r, &out);
 }
 
 static char *
@@ -238,6 +304,15 @@ ngx_http_casual(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
    cglcf->enable = 1;
 
    return NGX_CONF_OK ;
+}
+
+static void ngx_http_casual_input_post_read(ngx_http_request_t *r)
+{
+
+    ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
+                   "http post read request body");
+
+    r->read_event_handler = ngx_http_request_empty_handler;
 }
 
 static ngx_int_t ngx_http_casual_init(ngx_http_casual_loc_conf_t *cglcf)
@@ -272,4 +347,14 @@ ngx_http_casual_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
       ngx_http_casual_init(conf);
 
    return NGX_CONF_OK ;
+}
+
+char* handleError(ngx_http_request_t *r)
+{
+   long size = strlen(tperrnostring(tperrno));
+   char* error = ngx_pcalloc(r->pool, size);
+   strncpy(error, tperrnostring(tperrno), size);
+   ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, error);
+   r->headers_out.status = NGX_HTTP_BAD_REQUEST;
+   return error;
 }
