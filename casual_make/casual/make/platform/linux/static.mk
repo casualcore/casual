7270aacb9a8cfcb428bd5b0f73a8e6135346369e
Lazan <laz@laz.se>
2014-09-25 10:13:39 +0200

    linux make stuff

--- casual_make/casual/make/platform/linux/static.mk
+++ casual_make/casual/make/platform/linux/static.mk
@@ -16,30 +16,30 @@ DEFAULT_LIBS :=
 ##
 ######################################################################
 
-THIS_MAKEFILE = $(CURDIR)/$(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST))
-
 
 
 COMPILER = g++
 CROSSCOMPILER = clang++
 
 #
-# Vi �r beroende av att vi anv�nder bash
-#
-SHELL = bash
-
-#
-# Vi s�rskiljer mellan lib och exe enbart f�r purify och co.
+# Linkers
 #
 LIBRARY_LINKER = g++
 ARCHIVE_LINKER = ar rcs
+
+
+#
+# We make sure we use bash
+#
+SHELL = bash
+
 
 ifndef EXECUTABLE_LINKER
 EXECUTABLE_LINKER = g++
 endif
 
 #
-# Nu stter vi ihop de faktiska kompilator/lnk-direktiven
+# Compile and link directives
 #
 ifdef DEBUG
    COMPILE_DIRECTIVES = -g -pthread -c  -fpic -Wall -pedantic -Wno-long-long -Wno-variadic-macros -std=c++11
@@ -72,12 +72,6 @@ CROSS_COMPILE_DIRECTIVES = -g -c -Wall -pedantic -fcolor-diagnostics -Wno-long-l
 BUILDSERVER = casual-build-server -c $(EXECUTABLE_LINKER) 
 BUILDCLIENT = CC='$(EXECUTABLE_LINKER)' $(CASUALMAKE_PATH)/bin/buildclient -v
 
-#
-# Purify
-#
-PURIFYOPTIONS = -always-use-cache-dir=yes -lazy-load=no
-PURIFY_LINKER = purify purecov $(PURIFYOPTIONS) $(EXECUTABLE_LINKER) -lc -R$(TUXLIB_DIR)
-QUANTIFY_LINKER = quantify -always-use-cache-dir $(EXECUTABLE_LINKER) -R$(TUXLIB_DIR)
 
 #
 # VALGRIND
@@ -88,43 +82,24 @@ else
 VALGRIND_CONFIG=
 endif
 
-#
-# Anv�ndarens definerade paths l�ggs f�rst f�r ha f�retr�de
-# f�re default.
-# DB2 m�ste inkluderas f�re Tuxedo.
-#
-INCLUDE_PATHS := $(INCLUDE_PATHS) 
-BASE_LIBRARY_PATHS := $(LIBRARY_PATHS) /lib64 /usr/lib64 /usr/local/lib64
 
 #
-# Default include/library-paths
-#
-DEFAULT_INCLUDE_PATHS := ./inc 
-BASE_DEFAULT_LIBRARY_PATHS := ./bin  
-
-
-#
-# S�tt inledande v�xel f�r kompilatorn och l�nkaren.
+# Format the include-/library- paths
 # 
 LIBRARY_PATH_DIRECTIVE=-Wl,-rpath-link=
-#
-# S�tt inledande v�xel f�r kompilatorn och l�nkaren.
-# 
+
 INCLUDE_PATHS := $(addprefix -I, $(INCLUDE_PATHS) )
 LIBRARY_PATHS := $(addprefix -L, $(BASE_LIBRARY_PATHS) ) $(addprefix $(LIBRARY_PATH_DIRECTIVE), $(BASE_LIBRARY_PATHS) )
 DEFAULT_INCLUDE_PATHS := $(addprefix -I, $(DEFAULT_INCLUDE_PATHS) )
 DEFAULT_LIBRARY_PATHS := $(addprefix -L, $(BASE_DEFAULT_LIBRARY_PATHS) ) $(addprefix $(LIBRARY_PATH_DIRECTIVE), $(BASE_DEFAULT_LIBRARY_PATHS) )
 
 
+#
+# Header dependency stuff
+#
 HEADER_DEPENDENCY_COMMAND = -g++ -MP -MM -std=c++11
 
 
-
-######################################################################
-## 
-## Det transformerade inneh�llet i imakefilen f�ljer:
-##
-######################################################################
     
    
 
