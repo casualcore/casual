9af122534c0dbfa5d3105508405fecdfaf780ce3
Hans Bergkvist <hans.bergkvist@gmail.com>
2016-04-10 11:08:34 +0200

    Handling config for nginx

--- thirdparty/setup/install_nginx.py
+++ thirdparty/setup/install_nginx.py
@@ -13,7 +13,7 @@ URL="http://nginx.org/download/"
 FILENAME="nginx-1.8.1.tar.gz"
 TMP="/tmp/"
 
-if not os.getenv("CASUAL_BUILD_HOME") or not os.getenv("CASUAL_HOME"):
+if not os.getenv("CASUAL_BUILD_HOME") or not os.getenv("CASUAL_HOME") or not os.getenv("CASUAL_DOMAIN_HOME"):
 	raise SystemError, "CASUAL-environment need to be set"
 
 BASENAME=os.path.splitext(os.path.splitext(FILENAME)[0])[0]
@@ -30,18 +30,21 @@ if not os.path.exists(TMP + BASENAME):
 
 os.chdir(TMP + BASENAME)
 
+prefix = os.getenv('CASUAL_HOME', '/usr/local/casual') + '/nginx'
+
 print("Start setting up: " + BASENAME)
 print("Running configure")
 print( subprocess.check_output(['./configure',
 '--with-debug',
 '--with-cc-opt=-Wno-deprecated',
-'--prefix=' + os.getenv('CASUAL_HOME', '/usr/local/casual') + '/nginx' ,
+'--prefix=' + prefix,
 '--add-module=' + os.getenv('CASUAL_BUILD_HOME') + '/plugin',
 '--without-http_rewrite_module']))
 print("Running make")
 print( subprocess.check_output(['make']))
 print("Running install")
 print( subprocess.check_output(['make', 'install']))
-	
-
-	
+print("Updating configuration")
+print( subprocess.check_output(['sed', '-i','.tmp', 's@__CASUAL_DOMAIN_HOME__@' + os.getenv("CASUAL_DOMAIN_HOME") +'@', os.getenv("CASUAL_BUILD_HOME") + '/../thirdparty/nginx/nginx.conf']))
+os.rename(os.getenv("CASUAL_BUILD_HOME") + '/../thirdparty/nginx/nginx.conf.tmp', prefix + '/conf/nginx.conf') 
+print("Done")
