b2db14606a3d6b6bc351b0dcbc938d2dfad8a289
Kristian Ivarsson <kristone@hotmail.com>
2016-04-16 13:21:58 +0200

    This commit fixes #25 with some runtime tests in the unittest (or else it issues a warning)

--- middleware/common/unittest/isolated/source/test_transcode.cpp
+++ middleware/common/unittest/isolated/source/test_transcode.cpp
@@ -30,7 +30,7 @@ namespace casual
 
    namespace common
    {
-      TEST( casual_common_transcode, base64_encode)
+      TEST( casual_common_transcode_base64, encode)
       {
          EXPECT_TRUE( transcode::base64::encode( std::string( "")) == "");
          EXPECT_TRUE( transcode::base64::encode( std::string( "A")) == "QQ==");
@@ -39,7 +39,7 @@ namespace casual
          EXPECT_TRUE( transcode::base64::encode( std::string( "ABCD")) == "QUJDRA==");
       }
 
-      TEST( casual_common_transcode, base64_decode)
+      TEST( casual_common_transcode_base64, decode)
       {
          EXPECT_TRUE( transcode::base64::decode( "") == local::from_string( ""));
          EXPECT_TRUE( transcode::base64::decode( "QQ==") == local::from_string( "A"));
@@ -48,51 +48,70 @@ namespace casual
          EXPECT_TRUE( transcode::base64::decode( "QUJDRA==") == local::from_string( "ABCD"));
       }
 
-      // TODO: Enable these tests whenever iconv -l shows that ISO-8859-15 is present on Suse
-      TEST( DISABLED_casual_common_transcode, UT8_encode_euro_sign)
+      TEST( casual_common_transcode_utf8, test_existene_of_bogus_codeset__expecting_false)
       {
-         //const auto& closure = []( const unsigned char c){std::clog << static_cast<short>(c) << std::endl;};
+         EXPECT_FALSE( transcode::utf8::exist( "casual"));
+      }
 
+      TEST( casual_common_transcode_utf8, test_existene_of_utf8_codeset__expecting_true)
+      {
+         // this codeset should exist (or if not we're screwed)
+         EXPECT_TRUE( transcode::utf8::exist( "UTF8"));
+      }
+
+      TEST( casual_common_transcode_utf8, encode_euro_sign)
+      {
+         if( transcode::utf8::exist( "ISO-8859-15"))
          {
             const std::string source = { static_cast<std::string::value_type>(0xA4)};
             const std::string expect( u8"€");
             const std::string result = transcode::utf8::encode( source, "ISO-8859-15");
             EXPECT_TRUE( result == expect);
          }
-
+         else
+         {
+            GTEST_LOG_(WARNING);
+         }
       }
 
-      // TODO: Enable these tests whenever iconv -l shows that ISO-8859-15 is present on Suse
-      TEST( DISABLED_casual_common_transcode, UT8_decode_euro_sign)
+      TEST( casual_common_transcode_utf8, decode_euro_sign)
       {
-         //const auto& closure = []( const unsigned char c){std::clog << static_cast<short>(c) << std::endl;};
-
-         // €
-         const std::string source( u8"\u20AC");
-         const std::string expect = { static_cast<std::string::value_type>(0xA4)};
-         const std::string result = transcode::utf8::decode( source, "ISO-8859-15");
-         EXPECT_TRUE( result == expect);
-
+         if( transcode::utf8::exist( "ISO-8859-15"))
+         {
+            const std::string source( u8"\u20AC");
+            const std::string expect = { static_cast<std::string::value_type>(0xA4)};
+            const std::string result = transcode::utf8::decode( source, "ISO-8859-15");
+            EXPECT_TRUE( result == expect);
+         }
+         else
+         {
+            GTEST_LOG_(WARNING);
+         }
       }
 
-      // TODO: gives warning from clang and gives failure on OSX with locale "UTF-8"
-      TEST( DISABLED_casual_common_transcode, UTF8_encode_exotic_characters)
+      TEST( casual_common_transcode_utf8, UTF8_encode_exotic_characters)
       {
-         const std::string source{ static_cast<std::string::value_type>(0xE5), static_cast<std::string::value_type>(0xE4), static_cast<std::string::value_type>(0xF6)};
-         const std::string expect{ u8"åäö"};
-         const std::string result = transcode::utf8::encode( source, "ISO-8859-1");
-         EXPECT_TRUE( result == expect);
+         if( transcode::utf8::exist( "ISO-8859-1"))
+         {
+            const std::string source{ static_cast<std::string::value_type>(0xE5), static_cast<std::string::value_type>(0xE4), static_cast<std::string::value_type>(0xF6)};
+            const std::string expect{ u8"åäö"};
+            const std::string result = transcode::utf8::encode( source, "ISO-8859-1");
+            EXPECT_TRUE( result == expect);
+         }
+         else
+         {
+            GTEST_LOG_(WARNING);
+         }
       }
 
-
-      TEST( casual_common_transcode, hex_encode)
+      TEST( casual_common_transcode_hex, encode)
       {
          std::vector< std::uint8_t> binary{ 255, 0, 240, 10};
 
          EXPECT_TRUE( transcode::hex::encode( binary) == "ff00f00a") << "hex: " << transcode::hex::encode( binary);
       }
 
-      TEST( casual_common_transcode, hex_decode)
+      TEST( casual_common_transcode_hex, decode)
       {
          auto binary = transcode::hex::decode( "ff00f00a");
 
