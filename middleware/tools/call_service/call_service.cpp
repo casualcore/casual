435ead06080b227274a1a5de6e211de4b2bec80d
Kristian Ivarsson <kristone@hotmail.com>
2016-05-19 22:30:30 +0200

    Fixes #37 (with a few comments)

--- middleware/tools/call_service/call_service.cpp
+++ middleware/tools/call_service/call_service.cpp
@@ -75,9 +75,15 @@ int main( int argc, char* argv[])
             casual::common::argument::directive( { "-f", "--format"}, "(semi-optional) type of buffer " + types, type),
          }};
 
+         //
+         // TODO: See tickets #45 and #46
+         //
          parser.parse( argc, argv);
       }
 
+      //
+      // Read data from stdin
+      //
 
       std::string payload;
       while( std::cin.peek() != std::istream::traits_type::eof())
@@ -85,21 +91,38 @@ int main( int argc, char* argv[])
          payload.push_back( std::cin.get());
       }
 
+      //
+      // Check is user provided a type
+      //
+
       if( type.empty())
       {
+         //
+         // Try to deduce type
+         //
          type = type_from_input( payload);
       }
 
+      //
+      // Allocate a buffer
+      //
       auto buffer = tpalloc( type.c_str(), nullptr, payload.size());
 
       if( ! buffer)
       {
          throw std::runtime_error( tperrnostring( tperrno));
       }
+
+      //
+      // Copy payload to buffer
+      //
       std::memcpy( buffer, payload.data(), payload.size());
 
       long len = payload.size();
 
+      //
+      // Call the service
+      //
       const auto result = tpcall( service.c_str(), buffer, len, &buffer, &len, 0);
 
       const auto error = result == -1 ? tperrno : 0;
@@ -107,6 +130,10 @@ int main( int argc, char* argv[])
       payload.assign( buffer, len);
 
       tpfree( buffer);
+
+      //
+      // Check some result
+      //
 
       if( error)
       {
@@ -116,10 +143,17 @@ int main( int argc, char* argv[])
          }
       }
 
+      //
+      // Print the result
+      //
       std::cout << payload << std::endl;
+
 
       if( error)
       {
+         //
+         // Should only be with TPESVCFAIL
+         //
          throw std::logic_error( tperrnostring( error));
       }
 
