de211aab90ee2373280fc7e7020bf302c2e6f208
Fredrik Eriksson <laz@laz.se>
2017-02-07 21:12:50 +0100

    issue #66 combatibility issues with move-traits

--- middleware/common/include/common/traits.h
+++ middleware/common/include/common/traits.h
@@ -295,7 +295,7 @@ namespace casual
 
 
 
-#if __GNUC__ > 4 || __clang_major__ > 4
+#if __cplusplus > 201402L // vector will have notrhow move in c++17
          template< typename T>
          struct is_movable : std::integral_constant< bool,
             std::is_nothrow_move_constructible< T>::value && std::is_nothrow_move_assignable< T>::value> {};
