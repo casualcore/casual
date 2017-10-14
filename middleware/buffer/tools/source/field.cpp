765c4c0607a59519071aa0d0d62ce1d2c9b93e39
Kristian Ivarsson <kristone@hotmail.com>
2017-10-14 12:08:58 +0200

    Drop the illusion to let field-buffer-index reflect reality (insertion order) to become more FML-like and along with that become a bit more relaxed in iterations

--- middleware/buffer/tools/source/field.cpp
+++ middleware/buffer/tools/source/field.cpp
@@ -5,40 +5,12 @@
 //      Author: Kristone
 //
 
-#include <unordered_map>
-#include <string>
-
 #include "common/environment.h"
 #include "buffer/field.h"
+#include "buffer/internal/field.h"
 
 #include <stdexcept>
 #include <iostream>
-
-
-namespace casual
-{
-   namespace buffer
-   {
-      namespace field
-      {
-         namespace repository
-         {
-            //
-            // Defined in field.cpp
-            //
-
-            std::unordered_map<std::string,int> name_to_type();
-            std::unordered_map<int,std::string> type_to_name();
-
-            std::unordered_map<std::string,long> name_to_id();
-            std::unordered_map<long,std::string> id_to_name();
-         }
-
-      }
-
-   }
-
-}
 
 
 int main( int argc, char* argv[])
@@ -50,10 +22,10 @@ int main( int argc, char* argv[])
          casual::common::environment::variable::set( "CASUAL_FIELD_TABLE", argv[1]);
       }
 
-      const auto types = casual::buffer::field::repository::type_to_name();
+      const auto types = casual::buffer::field::internal::type_to_name();
 
 
-      const auto names = casual::buffer::field::repository::name_to_id();
+      const auto names = casual::buffer::field::internal::name_to_id();
 
 
       for( const auto& pair : names)
