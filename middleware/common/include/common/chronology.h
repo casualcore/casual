f5d260689406136ccee381ec2b9d2f8c61908fbb
Fredrik Eriksson <laz@laz.se>
2020-11-11 22:55:39 +0100

    build - renamed/missing library

--- middleware/common/include/common/chronology.h
+++ middleware/common/include/common/chronology.h
@@ -16,114 +16,108 @@
 
 namespace casual
 {
-   namespace common
+   namespace common::chronology
    {
-      namespace chronology
+      namespace utc
       {
-         namespace utc
-         {
-            //! Format a timepoint to iso 8601 extended date time with UTC offset
-            //! @{
-            void offset( std::ostream& out, platform::time::point::type time);
-            std::string offset( platform::time::point::type time);
-            //! @}
-         } // utc
+         //! Format a timepoint to iso 8601 extended date time with UTC offset
+         //! @{
+         void offset( std::ostream& out, platform::time::point::type time);
+         std::string offset( platform::time::point::type time);
+         //! @}
+      } // utc
 
-
-         namespace unit
-         {
-            namespace detail
-            {
-               template< typename D>
-               struct string;
-
-               template<>
-               struct string< std::chrono::nanoseconds> { constexpr static auto value = "ns";};
-
-               template<>
-               struct string< std::chrono::microseconds> { constexpr static auto value = "us";};
-
-               template<>
-               struct string< std::chrono::milliseconds> { constexpr static auto value = "ms";};
-
-               template<>
-               struct string< std::chrono::seconds> { constexpr static auto value = "s";};
-
-               template<>
-               struct string< std::chrono::minutes> { constexpr static auto value = "min";};
-
-               template<>
-               struct string< std::chrono::hours> { constexpr static auto value = "h";};
-
-               template<>
-               struct string< std::chrono::duration< double>> { constexpr static auto value = "s";};
-
-            }
-
-            template< typename D>
-            constexpr auto string( D&& duration) { return detail::string< std::decay_t< D>>::value;}
-         } // unit
-
-
-         namespace from
-         {
-            platform::time::unit string( const std::string& value);
-         } // from
-
-         namespace to
-         {
-            std::string string( std::chrono::nanoseconds duration);
-
-            template< typename R, typename P>
-            auto string( std::chrono::duration< R, P> duration) 
-            { 
-               return to::string( std::chrono::duration_cast< std::chrono::nanoseconds>( duration));
-            }
-         } // from
-
-
-         struct format
+      namespace unit
+      {
+         namespace detail
          {
             template< typename D>
-            void operator () ( std::ostream& out, D&& duration) const
-            {
-               out << duration.count() << unit::string( duration);
-            }
-         };
-
-         template< typename D>
-         std::string duration( D&& duration) 
-         { 
-            return std::to_string( duration.count()) + unit::string( duration);
-         }
-
-      } // chronology
-
-      namespace stream
-      {
-         namespace customization
-         {
-            template< typename R, typename P>
-            struct point< std::chrono::duration< R, P>>
-            {
-               template< typename D>
-               static void stream( std::ostream& out, D&& duration)
-               {
-                  out << duration.count() << chronology::unit::string( duration);
-               }
-            };
+            struct string;
 
             template<>
-            struct point< platform::time::point::type>
-            {
-               static void stream( std::ostream& out, const platform::time::point::type& time)
-               {
-                  chronology::utc::offset( out, time);
-               }
-            };
-         } // customization
+            struct string< std::chrono::nanoseconds> { constexpr static auto value = "ns";};
 
-      } // stream
-   } // common
+            template<>
+            struct string< std::chrono::microseconds> { constexpr static auto value = "us";};
+
+            template<>
+            struct string< std::chrono::milliseconds> { constexpr static auto value = "ms";};
+
+            template<>
+            struct string< std::chrono::seconds> { constexpr static auto value = "s";};
+
+            template<>
+            struct string< std::chrono::minutes> { constexpr static auto value = "min";};
+
+            template<>
+            struct string< std::chrono::hours> { constexpr static auto value = "h";};
+
+            template<>
+            struct string< std::chrono::duration< double>> { constexpr static auto value = "s";};
+
+         }
+
+         template< typename D>
+         constexpr auto string( D&& duration) { return detail::string< std::decay_t< D>>::value;}
+      } // unit
+
+
+      namespace from
+      {
+         platform::time::unit string( const std::string& value);
+      } // from
+
+      namespace to
+      {
+         std::string string( std::chrono::nanoseconds duration);
+
+         template< typename R, typename P>
+         auto string( std::chrono::duration< R, P> duration) 
+         { 
+            return to::string( std::chrono::duration_cast< std::chrono::nanoseconds>( duration));
+         }
+      } // to
+
+
+      struct format
+      {
+         template< typename D>
+         void operator () ( std::ostream& out, D&& duration) const
+         {
+            out << duration.count() << unit::string( duration);
+         }
+      };
+
+      template< typename D>
+      std::string duration( D&& duration) 
+      { 
+         return std::to_string( duration.count()) + unit::string( duration);
+      }
+
+   } // common::chronology
+
+   namespace common::stream::customization
+   {
+      template< typename R, typename P>
+      struct point< std::chrono::duration< R, P>>
+      {
+         template< typename D>
+         static void stream( std::ostream& out, D&& duration)
+         {
+            out << duration.count() << chronology::unit::string( duration);
+         }
+      };
+
+      template<>
+      struct point< platform::time::point::type>
+      {
+         static void stream( std::ostream& out, const platform::time::point::type& time)
+         {
+            chronology::utc::offset( out, time);
+         }
+      };
+
+   } // common::stream::customization
+
 } // casual
 
