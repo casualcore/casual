460aa77ef3249ebe204f232995bec8fc236f0ea4
Mikael Ivarsson <mikael.ivarsson4@gmail.com>
2020-11-17 17:30:45 +0100

    install script

--- console/setup/install.sh
+++ console/setup/install.sh
@@ -3,7 +3,11 @@
 echo 'Copying console files'
 python $CASUAL_BUILD_HOME/console/setup/install_console.py
 
+type pip
+whereis pip
+
+
 echo 'Running pip install'
-python -m pip install --no-cache-dir --target=${CASUAL_HOME}/console/server/lib -r ${CASUAL_HOME}/console/server/requirements.txt
+cd ${CASUAL_HOME}/console/server && pip install --no-cache-dir --target=${CASUAL_HOME}/console/server/lib -r ${CASUAL_HOME}/console/server/requirements.txt
 
 echo 'Console installed'
