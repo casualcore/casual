865584d837ccafb770687826b2aeaa4d144582a4
Fredrik Eriksson <laz@laz.se>
2021-07-14 15:15:29 +0200

    http - trying to "fix" nginx build
    I think the nginx stuff should also be under http/inbound/ and we
    get rid of thirdparty and middleware/plugin. It's just an
    implementation detail for http/inbound, or rather, should be.

--- thirdparty/setup/install_nginx.py
+++ thirdparty/setup/install_nginx.py
@@ -3,7 +3,7 @@
 # Usage: python install_nginx.py
 #
 
-import urllib
+import urllib.request
 import os
 import tarfile
 import subprocess
@@ -14,20 +14,19 @@ URL="http://nginx.org/download/"
 FILENAME="nginx-1.18.0.tar.gz"
 TMP="/tmp/"
 
-if not os.getenv("CASUAL_BUILD_HOME") or not os.getenv("CASUAL_HOME"):
-	raise SystemError("CASUAL-environment need to be set")
+if not os.getenv("CASUAL_MAKE_SOURCE_ROOT") or not os.getenv("CASUAL_HOME"):
+    raise SystemError("CASUAL-environment need to be set")
 
 BASENAME=os.path.splitext(os.path.splitext(FILENAME)[0])[0]
 
 if not os.path.exists(TMP + BASENAME):
-	print("Fetching: " + URL + FILENAME)
-	response = urllib.urlopen(URL + FILENAME)
-
-	with open(TMP + FILENAME, "wb") as f:
-		f.write(response.read())
-	
-	archive = tarfile.open(TMP + FILENAME, "r:gz")
-	archive.extractall(TMP)
+    print("Fetching: " + URL + FILENAME)
+    with urllib.request.urlopen( URL + FILENAME) as response:
+        with open(TMP + FILENAME, "wb") as file:
+            file.write( response.read())
+    
+    archive = tarfile.open( TMP + FILENAME, "r:gz")
+    archive.extractall(TMP)
 
 os.chdir(TMP + BASENAME)
 
@@ -39,12 +38,12 @@ print( subprocess.check_output(['./configure',
 '--with-debug',
 '--with-cc-opt=-Wno-deprecated',
 '--prefix=' + prefix,
-'--add-module=' + os.getenv('CASUAL_BUILD_HOME') + '/middleware/plugin',
+'--add-module=' + os.getenv('CASUAL_MAKE_SOURCE_ROOT') + '/middleware/plugin',
 '--without-http_rewrite_module']))
 print("Running make")
 print( subprocess.check_output(['make']))
 print("Running install")
 print( subprocess.check_output(['make', 'install']))
 print("Updating configuration")
-copyfile(os.getenv("CASUAL_BUILD_HOME") + '/thirdparty/nginx/nginx.conf', prefix + '/conf/nginx.conf') 
+copyfile(os.getenv("CASUAL_MAKE_SOURCE_ROOT") + '/thirdparty/nginx/nginx.conf', prefix + '/conf/nginx.conf') 
 print("Done")
