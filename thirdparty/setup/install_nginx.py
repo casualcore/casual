60a930ce33b36143368f45e90b77acde47a041c2
Hans Bergkvist <hans.bergkvist@forsakringskassan.se>
2023-03-31 16:58:06 +0200

    build - use local copy of nginx
    To be able to build nginx offline nginx is now stored in casual-thirdparty.
    This is some changes to be able to build towards that local copy.

--- thirdparty/setup/install_nginx.py
+++ thirdparty/setup/install_nginx.py
@@ -3,37 +3,25 @@
 # Usage: python install_nginx.py
 #
 
-import urllib.request
 import os
-import tarfile
 import subprocess
 
 from shutil import copyfile
 
-URL="http://nginx.org/download/"
-FILENAME="nginx-1.22.0.tar.gz"
-TMP="/tmp/"
+NGNIX_VERSION = "nginx-1.22.1"
 SOURCE_ROOT = os.getenv( "CASUAL_MAKE_SOURCE_ROOT", os.getenv("CASUAL_BUILD_HOME"))
+NGINX_ROOT = SOURCE_ROOT + '/../casual-thirdparty/nginx/' + NGNIX_VERSION + '/'
 
 if not SOURCE_ROOT or not os.getenv("CASUAL_HOME"):
 	raise SystemError("CASUAL-environment need to be set")
 
-BASENAME=os.path.splitext(os.path.splitext(FILENAME)[0])[0]
-
-if not os.path.exists(TMP + BASENAME):
-	print("Fetching: " + URL + FILENAME)
-	with urllib.request.urlopen( URL + FILENAME) as response:
-		with open(TMP + FILENAME, "wb") as f:
-			f.write(response.read())
-	
-	archive = tarfile.open(TMP + FILENAME, "r:gz")
-	archive.extractall(TMP)
-
-os.chdir(TMP + BASENAME)
+if not os.path.exists(NGINX_ROOT):
+   raise SystemError("NGINX-version " + NGINX_VERSION + " not found!")
 
 prefix = os.getenv('CASUAL_HOME', '/usr/local/casual') + '/nginx'
 
-print("Start setting up: " + BASENAME)
+os.chdir( NGINX_ROOT)
+print("Start setting up: " + NGINX_ROOT)
 print("Running configure")
 print( subprocess.check_output(['./configure',
 '--with-debug',
