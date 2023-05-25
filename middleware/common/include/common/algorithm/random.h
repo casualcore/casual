84b2996297672ee1bf326597fb46ea2bd05ebd7f
Fredrik Eriksson <laz@laz.se>
2023-05-25 13:02:11 +0200

    gateway: outbound order of connection for a service gets too "orderly"
    Before: The order of connections a given service has (the remote domains
    that expose the service) is based on the order which the connections
    connect. In some topologies this could give a bad load balancing.
    
    Now: When we add add a connection to a service we insert it a random
    position. round-robin is used as before after that.
    
    Resolves #201

---
+++ middleware/common/include/common/algorithm/random.h
@@ -0,0 +1,29 @@
+//!
+//! Copyright (c) 2023, The casual project
+//!
+//! This software is licensed under the MIT license, https://opensource.org/licenses/MIT
+//!
+
+#pragma once
+
+#include <random>
+
+namespace casual
+{
+   namespace common::algorithm::random
+   {
+      std::mt19937& generator() noexcept;
+
+      //! inserts `value` at a random position in `container`
+      template< typename C, typename V>
+      C& insert( C& container, V&& value)
+      {
+         std::uniform_int_distribution< decltype( container.size())> distribution{ 0, container.size()};
+
+         container.insert( std::begin( container) + distribution( random::generator()), std::forward< V>( value));
+
+         return container;
+      }
+      
+   } // common::algorithm::random
+} // casual
