535496b75dd4510febb8256f3e1bd0618b1dc81e
Fredrik Eriksson <laz@laz.se>
2023-11-27 21:02:51 +0100

    transaction: made transaction::global::ID hashable

--- middleware/common/include/common/transaction/global.h
+++ middleware/common/include/common/transaction/global.h
@@ -31,18 +31,45 @@ namespace casual::common
          inline friend bool operator == ( const common::transaction::ID& lhs, const ID& rhs) { return rhs == lhs;}
 
          CASUAL_CONST_CORRECT_SERIALIZE(
-         {
             CASUAL_SERIALIZE_NAME( m_size, "size");
             CASUAL_SERIALIZE_NAME( m_gtrid, "gtrid");
-         })
+         )
+
+         auto span() const noexcept
+         {
+            return std::span< const char>( m_gtrid.data(), m_size);
+         }
 
          friend std::ostream& operator << ( std::ostream& out, const ID& value);
          friend std::istream& operator >> ( std::istream& in, common::transaction::global::ID& gtrid);
 
       private:
-         std::int8_t m_size{};
-         std::array<char, 64> m_gtrid{};           
+         std::uint8_t m_size{};
+         std::array< char, 64> m_gtrid{};           
       };
+
+      namespace to
+      {
+         common::transaction::ID trid( const global::ID& gtrid);
+      } // to
+
+      
       
    } // transaction::global
 } // casual::common
+
+namespace std 
+{
+   template<>
+   struct hash< casual::common::transaction::global::ID>
+   {
+      auto operator()( const casual::common::transaction::global::ID& value) const noexcept
+      {
+         auto span = value.span();
+         
+         // we just use the string_view hash function.
+         auto string_representation = std::string_view{ span.data(), span.size()};
+         return std::hash< std::string_view>{}( string_representation);
+      }
+   };
+}
