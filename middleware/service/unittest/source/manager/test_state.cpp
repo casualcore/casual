5666b44f102f870e6e6613eb25711cd39088d498
Fredrik Eriksson <laz@laz.se>
2024-01-30 11:15:21 +0100

    service: use ipc id for remote instances
    This is a preparation when gateway-outbound starts using a dedicated
    ipc-device for each tcp-connection.
    
    issue #238

--- middleware/service/unittest/source/manager/test_state.cpp
+++ middleware/service/unittest/source/manager/test_state.cpp
@@ -48,10 +48,7 @@ namespace casual
 
          auto state = local::state();
 
-         {
-            common::message::service::Advertise message;
-            EXPECT_TRUE( state.update( message).empty());
-         }
+         EXPECT_TRUE( state.update( common::message::service::Advertise{}).empty());
 
          EXPECT_TRUE( state.instances.sequential.empty()) << CASUAL_NAMED_VALUE( state.instances.sequential);
          EXPECT_TRUE( state.instances.concurrent.empty());
@@ -63,10 +60,7 @@ namespace casual
 
          auto state = local::state();
 
-         {
-            common::message::service::concurrent::Advertise message;
-            EXPECT_TRUE( state.update( message).empty());
-         }
+         EXPECT_TRUE( state.update( common::message::service::concurrent::Advertise{}).empty());
 
          EXPECT_TRUE( state.instances.sequential.empty());
          EXPECT_TRUE( state.instances.concurrent.empty()) << CASUAL_NAMED_VALUE( state.instances.concurrent);
@@ -82,7 +76,7 @@ namespace casual
             common::message::service::Advertise message;
             message.process = common::process::handle();
             message.services.add.emplace_back( "service1");
-            EXPECT_TRUE( state.update( message).empty());
+            EXPECT_TRUE( state.update( std::move( message)).empty());
          }
 
          EXPECT_TRUE( state.instances.sequential.size() == 1);
@@ -90,8 +84,8 @@ namespace casual
             auto service = state.service( "service1");
             ASSERT_TRUE( service);
             EXPECT_TRUE( service->information.name == "service1");
-            ASSERT_TRUE( service->instances.sequential.size() == 1);
-            auto& instance = service->instances.sequential.at( 0);
+            ASSERT_TRUE( service->instances.sequential().size() == 1);
+            auto& instance = service->instances.sequential().at( 0);
             EXPECT_TRUE( instance.process() == common::process::handle());
             EXPECT_TRUE( instance.idle());
             EXPECT_TRUE( instance.get().service( "service1"));
@@ -111,7 +105,7 @@ namespace casual
             common::message::service::Advertise message;
             message.process = common::process::handle();
             message.services.add.emplace_back( "service1");
-            EXPECT_TRUE( state.update( message).empty());
+            EXPECT_TRUE( state.update( std::move( message)).empty());
          }
 
          // unadvertise
@@ -119,19 +113,19 @@ namespace casual
             common::message::service::Advertise message;
             message.process = common::process::handle();
             message.services.remove.emplace_back( "service1");
-            EXPECT_TRUE( state.update( message).empty());
+            EXPECT_TRUE( state.update( std::move( message)).empty());
          }
 
          {
             auto service = state.service( "service1");
             ASSERT_TRUE( service);
             EXPECT_TRUE( service->information.name == "service1");
-            EXPECT_TRUE( service->instances.sequential.size() == 0);
+            EXPECT_TRUE( service->instances.sequential().size() == 0);
          }
 
          {
 
-            auto instance = state.sequential( common::process::id());
+            auto instance = state.sequential( common::process::handle().ipc);
             ASSERT_TRUE( instance);
             EXPECT_TRUE( instance->process == common::process::handle());
             EXPECT_TRUE( instance->idle());
@@ -157,11 +151,11 @@ namespace casual
                std::shuffle( std::begin( message.services.add), std::end( message.services.add), generator);
             }
 
-            EXPECT_TRUE( state.update( message).empty());
+            EXPECT_TRUE( state.update( std::move( message)).empty());
          }
 
          {
-            auto instance = state.sequential( common::process::id());
+            auto instance = state.sequential( common::process::handle().ipc);
             ASSERT_TRUE( instance);
 
             for( auto& service : message.services.add)
@@ -189,7 +183,7 @@ namespace casual
                std::shuffle( std::begin( message.services.add), std::end( message.services.add), generator);
             }
 
-            EXPECT_TRUE( state.update( message).empty());
+            EXPECT_TRUE( state.update( std::move( message)).empty());
          }
 
          // unadvertise
@@ -197,19 +191,19 @@ namespace casual
             common::message::service::Advertise message;
             message.process = common::process::handle();
             message.services.remove = { "s4", "s7"};
-            EXPECT_TRUE( state.update( message).empty());
+            EXPECT_TRUE( state.update( std::move( message)).empty());
          }
          {
             auto service = state.service( "s4");
             ASSERT_TRUE( service);
-            EXPECT_TRUE( service->instances.sequential.empty());
+            EXPECT_TRUE( service->instances.sequential().empty());
             service = state.service( "s7");
             ASSERT_TRUE( service);
-            EXPECT_TRUE( service->instances.sequential.empty());
+            EXPECT_TRUE( service->instances.sequential().empty());
          }
 
          {
-            auto instance = state.sequential( common::process::id());
+            auto instance = state.sequential( common::process::handle().ipc);
             ASSERT_TRUE( instance);
             EXPECT_FALSE( instance->service( "s4"));
             EXPECT_FALSE( instance->service( "s7"));
@@ -217,6 +211,56 @@ namespace casual
 
          }
       }
+
+      TEST( service_manager_state, concurrent_instance_order)
+      {
+         common::unittest::Trace trace;
+
+         using Property = state::service::instance::Concurrent::Property;
+         using property_type = decltype( Property::type);
+
+         auto create_advertise = []( common::strong::process::id pid, platform::size::type order, Property property)
+         {
+            common::message::service::concurrent::Advertise result;
+            result.order = order;
+            result.process.pid = pid;
+            result.process.ipc = common::strong::ipc::id{ common::uuid::make()};
+            {
+               auto& service = result.services.add.emplace_back();
+               service.name = "a";
+               service.property = property;
+            }
+            return result;
+         };
+
+         manager::State state;
+
+         (void)state.update( create_advertise( common::strong::process::id{ 100}, 0, Property{ property_type::discovered, 0}));
+         (void)state.update( create_advertise( common::strong::process::id{ 100}, 0, Property{ property_type::discovered, 0}));
+         (void)state.update( create_advertise( common::strong::process::id{ 100}, 0, Property{ property_type::discovered, 0}));
+         (void)state.update( create_advertise( common::strong::process::id{ 101}, 1, Property{ property_type::discovered, 0}));
+         (void)state.update( create_advertise( common::strong::process::id{ 101}, 1, Property{ property_type::discovered, 0}));
+         (void)state.update( create_advertise( common::strong::process::id{ 102}, 2, Property{ property_type::discovered, 0}));
+         (void)state.update( create_advertise( common::strong::process::id{ 103}, 3, Property{ property_type::configured, 3}));
+
+         auto service = state.service( "a");
+
+         ASSERT_TRUE( service);
+
+         // 103 should be prioritized
+         {
+            auto process = service->reserve_concurrent();
+            EXPECT_TRUE( process.pid == common::strong::process::id{ 103});
+            
+            // expect only 103 to be in the prioritized range
+            EXPECT_TRUE( process == service->reserve_concurrent());
+
+            // we remove the instance
+            service->remove( )
+         }
+
+      }
+
 
    } // service::manager
 } // casual
