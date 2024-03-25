394b32be4b841eaba8c6d95b43763e6cf6067a05
Fredrik Eriksson <laz@laz.se>
2024-03-25 09:15:21 +0100

    common: extended log::line to accept std::error_condition

--- middleware/common/include/common/log/line.h
+++ middleware/common/include/common/log/line.h
@@ -13,14 +13,20 @@ namespace casual
 {
    namespace common::log
    {
+      namespace detail
+      {
+         template< typename T>
+         concept code_like = std::is_error_code_enum_v< T> || std::is_error_condition_enum_v< T>;
+      } // detail
+
       template< typename... Args>
       void line( std::ostream& out, Args&&... args)
       {
          log::write( out, std::forward< Args>( args)..., '\n');
       }
 
-      template< typename Code, typename... Args>
-      void code( std::ostream& out, Code code, Args&&... args) requires std::is_error_code_enum_v< Code>
+      template< detail::code_like Code, typename... Args>
+      void code( std::ostream& out, Code code, Args&&... args)
       {
          if constexpr( sizeof...( Args) == 0)
             log::line( out, code);
@@ -29,23 +35,23 @@ namespace casual
       }
 
 
-      template< typename Code, typename... Args>
-      auto error( Code code, Args&&... args) requires std::is_error_code_enum_v< Code>
+      template< detail::code_like Code, typename... Args>
+      auto error( Code code, Args&&... args)
       {
          log::code( log::category::error, code, std::forward< Args>( args)...);
       }
 
       namespace verbose
       {
-         template< typename Code, typename... Args>
-         auto error( Code code, Args&&... args) requires std::is_error_code_enum_v< Code>
+         template< detail::code_like Code, typename... Args>
+         auto error( Code code, Args&&... args)
          {
             log::code( log::category::verbose::error, code, std::forward< Args>( args)...);
          }
       } // verbose
 
-      template< typename Code, typename... Args>
-      auto warning( Code code, Args&&... args) requires std::is_error_code_enum_v< Code>
+      template< detail::code_like Code, typename... Args>
+      auto warning( Code code, Args&&... args)
       {
          log::code( log::category::warning, code, std::forward< Args>( args)...);
       }
