7542e7c3cbcb640c0b0de4feeb70d3a30eb9a3e5
Albin Ågren <57344915+AlbsAgren@users.noreply.github.com>
2024-11-11 21:32:06 +0100

    build: simplify install script for http-inbound (#457)
    * Remove use of CASUAL_BUILD_HOME
    * Remove include mime.types from example nginx.conf
    
    Partially resolves #22

--- thirdparty/setup/install_nginx.py
+++ thirdparty/setup/install_nginx.py
@@ -11,24 +11,25 @@ from shutil import copyfile
 
 NGINX_VERSION = "1.22.1"
 BASENAME = "nginx-" + NGINX_VERSION
-SOURCE_ROOT = os.getenv( "CASUAL_MAKE_SOURCE_ROOT", os.getenv("CASUAL_BUILD_HOME"))
+SOURCE_ROOT = os.getenv("CASUAL_MAKE_SOURCE_ROOT")
 CASUAL_THIRDPARTY = os.getenv("CASUAL_THIRDPARTY")
 
 if not SOURCE_ROOT or not CASUAL_THIRDPARTY:
-	raise SystemError("CASUAL-environment need to be set")
+	raise SystemError("CASUAL_MAKE_SOURCE_ROOT and CASUAL_THIRDPARTY need to be set")
 
 os.chdir(CASUAL_THIRDPARTY + '/nginx/' + BASENAME)
 
 prefix = os.getenv('CASUAL_HOME', '/opt/casual') + '/nginx'
 
 print("Start setting up: " + BASENAME)
+
 print("Running configure")
 print( subprocess.check_output(['./configure',
-'--with-debug',
-'--with-cc-opt=-Wno-deprecated',
-'--prefix=' + prefix,
-'--add-module=' + SOURCE_ROOT + '/middleware/http/source/inbound/nginx/plugin',
-'--without-http_rewrite_module']).decode())
+   '--with-debug',
+   '--prefix=' + prefix,
+   '--with-cc-opt=-Wno-deprecated',
+   '--add-module=' + SOURCE_ROOT + '/middleware/http/source/inbound/nginx/plugin',
+   '--without-http_rewrite_module']).decode())
 print("Running make")
 print( subprocess.check_output(['make']).decode())
 print("Running install")
