2b228d67dbb76d532c23e06c62631c39de186322
Kristian Ivarsson <kristone@hotmail.com>
2026-01-06 13:35:19 +0100

    archive: Reintroducing INI archive in order to be backward compatible
    TOML is not INI compatible and thus .ini-files cannot be handled at all
    but after this commit we can handle .ini-files again but still limited
    data structures (exaclty as in 1.8)

---
+++ middleware/common/include/common/serialize/ini.h
@@ -0,0 +1,44 @@
+//! 
+//! Copyright (c) 2015, The casual project
+//!
+//! This software is licensed under the MIT license, https://opensource.org/licenses/MIT
+//!
+
+
+#pragma once
+
+
+#include "common/serialize/archive.h"
+#include "casual/platform.h"
+
+#include <string>
+#include <iosfwd>
+
+namespace casual
+{
+   namespace common
+   {
+      namespace serialize
+      {
+         namespace ini
+         {
+            namespace strict
+            {
+               serialize::Reader reader( const std::string& source);
+               serialize::Reader reader( std::istream& source);
+               serialize::Reader reader( const platform::binary::type& source);
+            } // strict
+
+            namespace relaxed
+            {
+               serialize::Reader reader( const std::string& source);
+               serialize::Reader reader( std::istream& source);
+               serialize::Reader reader( const platform::binary::type& source);
+            } // relaxed
+
+            serialize::Writer writer();
+
+         } // ini
+      } // serialize
+   } // common
+} // casual
